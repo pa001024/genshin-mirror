e25001dc93fce1d9b0a2fff46948fe7929869062
pa001024 <pa001024@qq.com>
2020-10-28 00:58:20 +0800

    build: init

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,8 @@ module.exports = {
     browser: true,
     node: true,
   },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: ['prettier'],
+  extends: ["@nuxtjs/eslint-config-typescript", "prettier", "prettier/vue", "plugin:prettier/recommended", "plugin:nuxt/recommended"],
+  plugins: ["prettier"],
   // add your custom rules here
   rules: {},
-}
+};
---
+++ components/Charactor.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="gsm-charactor"></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class Charactor extends Vue {
+  // start
+  mounted() {}
+}
+</script>
+
+<style lang="less" scoped>
+.gsm-charactor {
+  color: red;
+}
+</style>
