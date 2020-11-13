fa8ed96d85aeed710bc9305481f7b9612a515752
pa001024 <pa001024@qq.com>
2020-11-13 10:05:11 +0800

    docs: update readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,7 @@ module.exports = {
   extends: ["@nuxtjs/eslint-config-typescript", "prettier", "prettier/vue", "plugin:prettier/recommended", "plugin:nuxt/recommended"],
   plugins: ["prettier"],
   // add your custom rules here
-  rules: {},
+  rules: {
+    "no-unused-vars": 0,
+  },
 };
